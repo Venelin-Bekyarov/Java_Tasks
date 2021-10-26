3843ca2ac624a5b0fda7685bd44490b6bf6b8d86
Venelin-Bekyarov <venbeks@gmail.com>
2021-10-26 11:01:25 +0100

    26th of October

--- Chapter8/src/C8_01C.java
+++ Chapter8/src/C8_01C.java
@@ -8,15 +8,16 @@ public class C8_01C {
         int number2 = 35;
         int number3 = 43;
         int number4 = 251;
-        float number5 = (float) -0.41;
+        float number5 = -0.41f;
         float printNumber5 = number5;
-        System.out.printf("%s %n", number1);
-        System.out.printf("%s %n", number2);
-        System.out.printf("%s %n", number3);
-        System.out.printf("%s %n", number4);
+        System.out.println(Integer.toString(number1,2));
+        System.out.println(Integer.toString(number2,2));
+        System.out.println(Integer.toString(number3,2));
+        System.out.println(Integer.toString(number4,2));
+
 
         int i = 0;
-        char[] decimalNumbers = new char[32];
+        String[ ]numbs = new String[32];
         boolean isPositive = true;
         while (i < 32) {
             number5 *= 2;
@@ -26,11 +27,11 @@ public class C8_01C {
                 number5 *= -1;
             }
 
-            if (number5 - 1 > 0) {
+            if (number5-1 > 0) {
                 number5 -= 1;
-                decimalNumbers[i] = (char) 1;
+                numbs[i]="1";
             } else {
-                decimalNumbers[i] = (char) 0;
+                numbs[i]="0";
             }
             i++;
         }
@@ -39,8 +40,8 @@ public class C8_01C {
         }
         System.out.print("0.");
 
-        for (var digit : decimalNumbers) {
-            System.out.printf("%c", digit);
+        for (var digit : numbs) {
+            System.out.print(digit);
         }
         System.out.println();
     }
