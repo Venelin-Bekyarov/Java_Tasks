55ea6e31f8353ad37af6f34a7da6a52f919636e5
Venelin-Bekyarov <venbeks@gmail.com>
2021-10-31 20:52:49 +0000

    31st of October

---
+++ Chapter9/src/C9_06.java
@@ -0,0 +1,41 @@
+//Задача 6:
+//Напишете метод, който връща позицията на първия елемент на масив,
+//който е по-голям от двата свои съседи едновременно, или -1, ако няма
+//такъв елемент.
+
+
+import java.util.Scanner;
+
+public class C9_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("You will find an array's element that is greater than its adjacent elements simultaneously. %n" +
+                "Please describe how many elements you'll enter: ");
+
+        int arrayLength = Integer.parseInt(scanner.nextLine());
+
+
+        System.out.println("Please input the numbers: ");
+        int[] array = new int[arrayLength];
+        for (int indexes = 0; indexes < array.length; indexes++) {
+            array[indexes] = Integer.parseInt(scanner.nextLine());
+        }
+
+        int result = CheckAllNeighbors(array);
+        if (result != -1) System.out.printf("Found at position %d", result);
+        else System.out.println("Not found");
+    }
+
+    static int CheckAllNeighbors(int[] array) {
+        for (int i = 1; i < array.length - 1; i++) {
+            if (CheckNeighbors(array, i)) return i;
+        }
+        return -1;
+    }
+
+    static boolean CheckNeighbors(int[] array, int position) {
+        if (array[position] > array[position - 1] && array[position] > array[position + 1]) {
+            return true;
+        } else return false;
+    }
+}
