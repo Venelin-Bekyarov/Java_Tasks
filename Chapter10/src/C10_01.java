0b9cb9f8b5ec8dada68bf18a7a6929febfb869e3
Venelin-Bekyarov <venbeks@gmail.com>
2021-11-03 21:28:24 +0000

    3st of November

---
+++ Chapter10/src/C10_01.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class C10_01 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("You will find all repetitive combinations of 'K' elements over 'N'-elements aggregate. %n" +
+                "Please enter 'N': ");
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] arr = new int[n];
+        System.out.print("please enter 'K': ");
+        int k = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+        System.out.println("The combinations are: ");
+        combRepeating(arr, n, k);
+    }
+
+    static void combRepeating(int[] arr, int n, int k) {
+
+        int[] chosen = new int[k + 1];
+        combinationLookUp(chosen, arr, 0, k, 0, n - 1);
+    }
+
+    static void combinationLookUp(int[] tempStore, int[] mainArray, int index, int k, int start, int end) {
+
+        if (index == k) {
+            for (int i = 0; i < k; i++) {
+                System.out.printf("%d ", mainArray[tempStore[i]]);
+            }
+            System.out.printf("%n");
+            return;
+        }
+        for (int i = start; i <= end; i++) {
+            tempStore[index] = i;
+            combinationLookUp(tempStore, mainArray, index + 1, k, i, end);
+        }
+    }
+}
+
+
---
+++ Chapter10/src/C10_02.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class C10_02 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("You will find all repetitive variations of 'N' elements of 'K' class. %n" +
+                "Please enter 'N': ");
+        int n = Integer.parseInt(scanner.nextLine());
+        System.out.print("Please enter 'K': ");
+        int k = Integer.parseInt(scanner.nextLine());
+        System.out.println("The variations are: ");
+        int[] holder = new int[k];
+
+        chooseElement(n, k, 0, holder);
+    }
+
+    static void chooseElement(int n, int k, int index, int[] holder) {
+        if (index == k) {
+            holderPrint(k, holder);
+        } else {
+            for (int i = 1; i <= n; i++) {
+                holder[index] = i;
+                chooseElement(n, k, index + 1, holder);
+            }
+        }
+    }
+
+    static void holderPrint(int k, int[] holder) {
+        for (int i = 0; i < k; i++) {
+            System.out.print(holder[i]);
+            if (i < k - 1) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+}
