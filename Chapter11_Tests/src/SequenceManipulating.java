3a789cc5ff22fe740167ee5d943d0203e22b63a3
Venelin-Bekyarov <venbeks@gmail.com>
2021-11-09 20:25:00 +0000

    9th of November

---
+++ Chapter11_Tests/src/SequenceManipulating.java
@@ -0,0 +1,5 @@
+public class SequenceManipulating {
+        public static void main(String[] args) {
+            System.out.printf("Sequence[1..3]: %d, %d, %d%n", Sequence.nextValue(), Sequence.nextValue(), Sequence.nextValue());
+        }
+}
