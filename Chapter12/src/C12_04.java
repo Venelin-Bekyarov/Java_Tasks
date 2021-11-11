bf9cd8705011df319da4dd4be6edd4789784bd34
Venelin-Bekyarov <venbeks@gmail.com>
2021-11-11 18:50:40 +0000

    11th of November

---
+++ Chapter12/src/C12_04.java
@@ -0,0 +1,23 @@
+public class C12_04 {
+
+    /*
+    java.lang.IllegalArgumentException subclasses:
+
+    IllegalChannelGroupException,
+    IllegalCharsetNameException,
+    IllegalFormatException,
+    IllegalSelectorException,
+    IllegalThreadStateException,
+    InvalidKeyException,
+    InvalidOpenTypeException,
+    InvalidParameterException,
+    InvalidPathException,
+    KeyAlreadyExistsException,
+    NumberFormatException,
+    PatternSyntaxException,
+    ProviderMismatchException,
+    UnresolvedAddressException,
+    UnsupportedAddressTypeException,
+    UnsupportedCharsetException
+     */
+}
