a271a98671414b2c243bbd4b84dccdd5b4faad38
Venelin-Bekyarov <venbeks@gmail.com>
2021-11-11 17:48:35 +0000

    11th of November

---
+++ Chapter12/src/C12_02.java
@@ -0,0 +1,39 @@
+public class C12_02 {
+    /*
+
+    java.lang.RuntimeException subclasses:
+
+    ArithmeticException               -Arithmetic error, such as divide-by-zero.
+
+    ArrayIndexOutOfBoundsException    -Array index is out-of-bounds.
+
+    ArrayStoreException               -Assignment to an array element of an incompatible type.
+
+    ClassCastException                -Invalid cast.
+
+    IllegalArgumentException          -Illegal argument used to invoke a method.
+
+    IllegalMonitorStateException      -Illegal monitor operation, such as waiting on an unlocked thread.
+
+    IllegalStateException             -Environment or application is in incorrect state.
+
+    IllegalThreadStateException       -Requested operation not compatible with current thread state.
+
+    IndexOutOfBoundsException         -Some type of index is out-of-bounds.
+
+    NegativeArraySizeException        -Array created with a negative size.
+
+    NullPointerException              -Invalid use of a null reference.
+
+    NumberFormatException             -Invalid conversion of a string to a numeric format.
+
+    SecurityException                 -Attempt to violate security.
+
+    StringIndexOutOfBounds            -Attempt to index outside the bounds of a string.
+
+    TypeNotPresentException           -Type not found.
+
+    UnsupportedOperationException     -An unsupported operation was encountered.
+
+     */
+}
