98035736753eef93ec2fe0f1937d77dd2d92e845
Venelin-Bekyarov <venbeks@gmail.com>
2021-11-15 13:55:38 +0000

    15th of November

--- Chapter13/src/ExtractDateHourParts.java
+++ Chapter13/src/ExtractDateHourParts.java
@@ -10,5 +10,13 @@ public class ExtractDateHourParts {
         System.out.println(dateNow); // Sun Oct 12 15:57:39 EEST 2008
         String formattedDate = sdf.format(dateNow);
         System.out.println(formattedDate); // 15 часа и 57 мин.
+
+
+        /*String pattern = "EEEE, dd.MM.yyyy г.";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        Date dateNow = new Date();
+        System.out.println(dateNow); // Sun Oct 12 16:09:04 EEST 2008
+        String formattedDate = sdf.format(dateNow);
+        System.out.println(formattedDate); // Неделя, 12.10.2008 г.*/
     }
 }
