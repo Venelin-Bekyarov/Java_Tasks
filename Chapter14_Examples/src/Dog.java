5f6b1aaa2943a0c1062192f07726f0aab5519607
Venelin-Bekyarov <venbeks@gmail.com>
2021-11-17 22:05:27 +0000

    17th of November

---
+++ Chapter14_Examples/src/Dog.java
@@ -0,0 +1,18 @@
+public class Dog {
+    public String name = "Sharo";
+
+    public Dog() {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+//    public void bark() {
+//        System.out.println("wow-wow");
+//    }
+
+//    public void doSth() {
+//        this.bark();
+//    }
+}
