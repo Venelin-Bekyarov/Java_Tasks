fad6fcde06d28481af8916a56af8b2c5c8a96140
Venelin-Bekyarov <venbeks@gmail.com>
2021-11-21 22:13:48 +0000

    21st of November

---
+++ Chapter14_Examples/src/Test.java
@@ -0,0 +1,11 @@
+
+import java.time.LocalDateTime;
+
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println(LocalDateTime.now().getHour());
+        System.out.println(LocalDateTime.now().getMinute());
+    }
+}
